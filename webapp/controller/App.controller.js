4a2141ef7cf10c729ead3ffcad7bca8ed2c3bd6e
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-04-29 18:07:50 +0300

    step 8: i18n

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
     'sap/ui/model/json/JSONModel',
+    'sap/ui/model/resource/ResourceModel',
   ],
-  (Controller, MessageToast, JSONModel) => {
+  (Controller, MessageToast, JSONModel, ResourceModel) => {
     'use strict';
 
     return Controller.extend('ui5.walkthrough.controller.App', {
@@ -12,16 +13,31 @@ sap.ui.define(
         // set data model on view
         const oData = {
           recipient: {
-            name: 'World',
+            name: 'World !!!!!',
           },
         };
         //instantiate a JSON model
         const oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+
+        // set i18 model on view
+        const i18nModel = new ResourceModel({
+          bundleName: 'ui5.walkthrough.i18n.i18n',
+        });
+        this.getView().setModel(i18nModel, 'i18n');
       },
 
       onShowHello() {
-        MessageToast.show('hi em');
+        // read msg from i18n model
+        const oBundle = this.getView().getModel('i18n').getResourceBundle();
+        const sRecipient = this.getView()
+          .getModel()
+          .getProperty('/recipient/name');
+
+        const sMsg = oBundle.getText('helloMsg', [sRecipient, ' from Vietnam']); //helloMsg=Xin chao {0} {1}
+
+        // show message
+        MessageToast.show(sMsg);
       },
     });
   }
