26700ed4ad6d8d6ea68399a4598c075352a02918
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-04-29 21:52:39 +0300

    Step 10: Descriptor/ manifest.json

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,21 +1,13 @@
 sap.ui.define(
-  [
-    'sap/ui/core/UIComponent',
-    'sap/ui/model/json/JSONModel',
-    'sap/ui/model/resource/ResourceModel',
-  ],
-  (UIComponent, JSONModel, ResourceModel) => {
+  ['sap/ui/core/UIComponent', 'sap/ui/model/json/JSONModel'],
+  (UIComponent, JSONModel) => {
     'use strict';
 
     return UIComponent.extend('ui5.walkthrough.Component', {
       //The metadata section defines a reference to the root view,
       metadata: {
         interfaces: ['sap.ui.core.IAsyncContentCreation'],
-        rootView: {
-          viewName: 'ui5.walkthrough.view.App',
-          type: 'XML',
-          id: 'app',
-        },
+        manifest: 'json',
       },
 
       init() {
@@ -30,12 +22,6 @@ sap.ui.define(
         };
         const oModel = new JSONModel(oData);
         this.setModel(oModel);
-
-        // set i18n model
-        const i18nModel = new ResourceModel({
-          bundleName: 'ui5.walkthrough.i18n.i18n',
-        });
-        this.setModel(i18nModel, 'i18n');
       },
     });
   }
